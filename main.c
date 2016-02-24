8f82f1928d6c2c032286e66185ef311f05336c68
Johannes Löthberg <johannes@kyriasis.com>
2016-02-24 18:24:20 +0100

    main.c: Update for type-specific compare functions
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- main.c
+++ main.c
@@ -16,36 +16,58 @@ strnew(const char * string) {
 
 int
 main(void) {
-	hm_map * map = hm_new();
+	// Test a string-keyed hashmap
+	hm_map * map = hm_new(hm_hash_string, hm_cmp_string);
 
 	char * foo = strnew("foo");
 	char * bar = strnew("bar");
 	char * baz = strnew("baz");
-
 	char * one = strnew("one");
 	char * two = strnew("two");
 
-	hm_insert(map, foo, foo);
-	hm_insert(map, foo, bar);
-	hm_insert(map, foo, baz);
+	hm_insert(map, foo, 4, foo);
+	hm_insert(map, foo, 4, bar);
+	hm_insert(map, foo, 4, baz);
 
 
-	hm_insert(map, one, two);
-	hm_insert(map, one, one);
+	hm_insert(map, one, 4, two);
+	hm_insert(map, one, 4, one);
 
-	hm_insert(map, two, two);
+	hm_insert(map, two, 2, two);
 
 
 	void ** value = (void **)malloc(sizeof(char *));
+	*value = 0;
 
-	hm_find(map, foo, value);
+	hm_find(map, foo, 4, value);
 	printf("%s\n", *value);
 
-	hm_find(map, one, value);
+	hm_find(map, one, 2, value);
 	printf("%s\n", *value);
 
-	hm_find(map, two, value);
+	hm_find(map, two, 2, value);
 	printf("%s\n", *value);
+
+
+	void ** ivalue = (void **)malloc(sizeof(uint32_t *));
+	*ivalue = 0;
+
+
+	// Test an integer-keyed hashmap
+	hm_map * intmap = hm_new(hm_hash_int, hm_cmp_int);
+
+	uint32_t ione = 1;
+	uint32_t itwo = 2;
+
+	hm_insert(intmap, &ione, 32, foo);
+	hm_insert(intmap, &itwo, 32, bar);
+
+	hm_find(intmap, &ione, 32, ivalue);
+	printf("%s\n", *ivalue);
+
+	hm_find(intmap, &itwo, 32, ivalue);
+	printf("%s\n", *ivalue);
+
 
 	free(foo);
 	free(bar);
@@ -53,7 +75,10 @@ main(void) {
 	free(one);
 	free(two);
 	free(value);
+	free(ivalue);
 
 	free(map->data);
 	free(map);
+	free(intmap->data);
+	free(intmap);
 }
