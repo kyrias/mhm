ac2948abcd4f08d5224a6f8bc79a3756b824bdd5
Johannes Löthberg <johannes@kyriasis.com>
2016-02-24 01:31:32 +0100

    main.c: restructure
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- main.c
+++ main.c
@@ -19,31 +19,32 @@ main(void) {
 	hm_map * map = hm_new();
 
 	char * foo = strnew("foo");
-	hm_insert(map, "foo", foo);
-
 	char * bar = strnew("bar");
-	hm_insert(map, "foo", bar);
-
 	char * baz = strnew("baz");
-	hm_insert(map, "foo", baz);
-
 
 	char * one = strnew("one");
-	hm_insert(map, "1", one);
-
 	char * two = strnew("two");
-	hm_insert(map, "2", two);
+
+	hm_insert(map, foo, foo);
+	hm_insert(map, foo, bar);
+	hm_insert(map, foo, baz);
+
+
+	hm_insert(map, one, two);
+	hm_insert(map, one, one);
+
+	hm_insert(map, two, two);
 
 
 	void ** value = (void **)malloc(sizeof(char *));
 
-	hm_find(map, "foo", value);
+	hm_find(map, foo, value);
 	printf("%s\n", *value);
 
-	hm_find(map, "1", value);
+	hm_find(map, one, value);
 	printf("%s\n", *value);
 
-	hm_find(map, "2", value);
+	hm_find(map, two, value);
 	printf("%s\n", *value);
 
 	free(foo);
@@ -52,6 +53,7 @@ main(void) {
 	free(one);
 	free(two);
 	free(value);
+
 	free(map->data);
 	free(map);
 }
